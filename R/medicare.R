547f404aeddc5ff4e825e56d6fb43e1106a28a47
robertgambrel <robert.gambrel@gmail.com>
2016-09-04 12:10:40 -0500

    Make package help file

---
+++ R/medicare.R
@@ -0,0 +1,15 @@
+#' @title medicare: tools for simplifying Medicare data analysis
+#' @name medicare
+#'   
+#' @description The medicare package contains useful functions for manipulating
+#'   raw Medicare public use files. These sometimes come with SAS read-in code
+#'   provided, but more frequently require the analyst to manually recode and
+#'   rename variables based on thorough review of the data documentation. This
+#'   package focuses mostly on Cost Reports and Provider of Service files, but
+#'   more support will be added for other sources in the future.
+#'   
+#'   
+#' @docType package
+#' @aliases medicare medicare-package
+
+NULL
