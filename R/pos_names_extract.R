d420b802b74d58871cc9d6ac45708fcd965fd18e
robertgambrel <robert.gambrel@gmail.com>
2016-09-28 10:10:55 -0500

    Additional error checking

--- R/pos_names_extract.R
+++ R/pos_names_extract.R
@@ -86,8 +86,11 @@ pos_names_extract <- function(layout_file, data_file) {
   
   # reorder the variable so that the new names are in the same name as the old
   # names in the data frame
-  if (sum(colmatches$old_names %in% ordering) != length(ordering)) {
-    stop("Number of variables in layout file did not match number of variables in the dataset. Are you sure the layout file year and dataset year match?")
+  if (length(ordering) != length(colmatches$old_names)) {
+    stop("Number of variables in Layout file did not match number of variables in the dataset. Are you sure the layout file year and dataset year match?")
+  }
+  if (sum(colmatches$old_names %in% ordering) != length(colmatches$old_names)) {
+    stop("Generic variable names in Layout file do not match names in dataset. Are you sure the layout file year and dataset year match?")
   }
   colmatches <- colmatches[match(ordering, colmatches$old_names), ]
   
